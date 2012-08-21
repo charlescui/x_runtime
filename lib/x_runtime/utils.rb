5ebfd4a0f6353989df9d7c5bec32c8b606eb1257
崔峥 <zheng.cuizh@gmail.com>
2012-08-21 13:14:21 +0800

    remove array

--- lib/x_runtime/utils.rb
+++ lib/x_runtime/utils.rb
@@ -1,4 +1,22 @@
 module XRuntime
   module Utils
+    class << self
+      def to_hash(data)
+        case data
+        when Array
+          array_hashify.call(data)
+        end
+      end
+  
+      def array_hashify
+        lambda { |array|
+          hash = Hash.new
+          array.each_slice(2) do |field, value|
+            hash[field] = value
+          end
+          hash
+        }
+      end
+    end
   end
 end
